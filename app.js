78bdd05c94f6558e9254b1d52edbd5de00d507c4
habeshawit <shalomseleshi@gmail.com>
2021-09-19 12:50:54 -0700

    add bootstrap cards

--- app.js
+++ app.js
@@ -18,22 +18,23 @@ const displayData = data => {
     document.querySelector('#image-container').innerHTML = "";
     
     data.forEach(
-        photo => (document.querySelector('#image-container').innerHTML += `
-        
-        <div class="card-deck">
-            <div class="card" id="image-${photo.date}">
+        photo => (document.querySelector('.card-deck').innerHTML += `
+            <div class="card" id="image-${photo.date}" style="flex: 1 0 40%;">
                 <img class="card-img-top" src=${photo.hdurl} alt="Card image cap">
 
                 <div class="card-body">
                     <h5 class="card-title">${photo.title}</h5>
                     <p class="card-text">${photo.explanation}</p>
+                <button type="button" class="btn btn-primary">Like</button>
+
                 </div>
 
+                
                 <div class="card-footer">
                     <small class="text-muted">${photo.date}</small>
                 </div>
             </div>
-        </div>
+        
 
             
         `)
