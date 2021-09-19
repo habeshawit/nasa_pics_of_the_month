8055b57cf4756f6ac7be2c50215dd049f72254b7
habeshawit <shalomseleshi@gmail.com>
2021-09-19 14:13:42 -0700

    center header

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const fetchNASAData = async () => {
         const data = await response.json()
         console.log('NASA data', data)
         displayData(data)
+        
     } catch (error) {
         console.log(error)
     }
@@ -18,21 +19,33 @@ const displayData = data => {
     document.querySelector('#image-container').innerHTML = "";
     
     data.forEach(
-        photo => (document.querySelector('.card-deck').innerHTML += `
+        photo => {
+            like_count = 0;
+            (document.querySelector('.card-deck').innerHTML += `
             <div class="card" id="image-${photo.date}">
                 <img class="card-img-top" src=${photo.hdurl} alt="Image Unavailable" style="height:55vh!important">
                 <div class="card-body">
                     <h5 class="card-title">${photo.title} - ${photo.date}</h5>
                     <p class="card-text">${photo.explanation}</p>
-                <button type="button" class="btn btn-primary">Like</button>
-
+                    <button type="button" class="btn btn-primary" id="like-button-${photo.date}">Like</button>
                 </div>
 
             </div>
-     
         `)
-      );
+        }
+        
+    );
 
+    let buttons = document.querySelectorAll(".btn")
+
+    buttons.forEach(button => {
+        button.addEventListener('click', handleLikeClick);
+    })
+}
+
+const handleLikeClick = () =>{
+    debugger
+    console.log("like button clicked");
 }
 
 fetchNASAData()
